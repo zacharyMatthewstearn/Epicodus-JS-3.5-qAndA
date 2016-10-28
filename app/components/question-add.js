70d04aac3df3d4746799aeed973916b229363f5f
Zachary Matthewstearn <zammo911@gmail.com>
2016-10-28 10:57:13 -0700

    add functionality to add new questions

--- app/components/question-add.js
+++ app/components/question-add.js
@@ -8,6 +8,15 @@ export default Ember.Component.extend({
     },
     closeAddQuestionForm() {
       this.set('addQuestionFormIsOpen', false);
+    },
+    saveQuestion1() {
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        notes: this.get('notes')
+      };
+      this.set('addQuestionFormIsOpen', false);
+      this.sendAction('saveQuestion2', params);
     }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+  actions: {
+    saveQuestion3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+  }
 });
